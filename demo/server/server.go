2a0beeb5f5206e9ca8aec421bc6a6713dc3efb6f
薛悦榕 <xyuerong@163.com>
2022-06-04 20:58:53 +0800

    demo_server

--- demo/server/server.go
+++ demo/server/server.go
@@ -1,42 +1,91 @@
-package main
+package xmit
 
 import (
-	"strconv"
-	"strings"
+	"fmt"
+	"io"
+	"net/http"
+	"unsafe"
 
-	"e.coding.net/xverse-git/xmedia/xrtc/xvrtc/xmit"
+	"github.com/google/uuid"
 )
 
-var (
-	address = "175.178.27.150"
-	demo    xmit.DemoServer
+type (
+	DemoServer struct {
+		server        *Server
+		callbacks     *ServerCallbacks
+		serverAddress uint32
+		dataStream    *ServerStream
+		ctrlStream    *ServerStream
+	}
 )
 
-func IPV4ToUint(ip string) uint32 {
-	ip = strings.TrimSpace(ip)
-	bits := strings.Split(ip, ".")
-	if len(bits) != 4 {
-		return 0
+func (demo *DemoServer) signalHttp(rsp http.ResponseWriter, req *http.Request) {
+	req.Close = true
+	offer, _ := io.ReadAll(req.Body)
+
+	rsp.Header().Set("Content-Type", "application/octet-stream")
+	rsp.Header().Set("Access-Control-Allow-Origin", "*")
+	rsp.Header().Set("Access-Control-Allow-Credentials", "true")
+	rsp.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+	rsp.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+
+	if _, err := rsp.Write(demo.Accept(offer)); err != nil {
+		fmt.Println("HTTP ERR:", err)
 	}
-
-	b0, _ := strconv.Atoi(bits[0])
-	b1, _ := strconv.Atoi(bits[1])
-	b2, _ := strconv.Atoi(bits[2])
-	b3, _ := strconv.Atoi(bits[3])
-	if b0 > 255 || b1 > 255 || b2 > 255 || b3 > 255 {
-		return 0
-	}
-
-	var sum uint32
-
-	sum += uint32(b0) << 24
-	sum += uint32(b1) << 16
-	sum += uint32(b2) << 8
-	sum += uint32(b3)
-
-	return sum
 }
 
-func main() {
-	demo.Start(IPV4ToUint(address))
+func (demo *DemoServer) echoHandler(connId []byte, msgType uint16, data []byte) {
+	switch msgType {
+	case 1:
+		demo.ctrlStream.Send(data)
+		demo.server.Close(connId)
+		fmt.Println("Close Connection:", string(connId))
+	case 2:
+		demo.dataStream.Send(data)
+		seq := *(*uint64)(unsafe.Pointer(&data[0]))
+		fmt.Println("Msg: (", seq, ", TYPE: ", msgType, ", SIZE: ", len(data), ") Echoed")
+	}
+}
+
+func (demo *DemoServer) connectedHandler(connId []byte) {
+	fmt.Println("Connected!")
+}
+
+func (demo *DemoServer) Start(serverAddress uint32) {
+	http.HandleFunc("/signal", demo.signalHttp)
+	go http.ListenAndServe(":51913", nil)
+
+	demo.serverAddress = serverAddress
+	demo.server, _ = GetServer(serverAddress, 0, 0)
+	if demo.server.rawServer == nil {
+		fmt.Println("fail to Init Xmit Server")
+	}
+	demo.callbacks = &ServerCallbacks{
+		OnMessage:   demo.echoHandler,
+		OnConnected: demo.connectedHandler,
+	}
+
+	Start()
+
+	signalServerAddress = serverAddress
+	go func(demo *DemoServer) {
+		for {
+			getSignal().sendAnswer(demo.Accept(getSignal().recvOffer()))
+		}
+	}(demo)
+
+	Join()
+}
+
+func (demo *DemoServer) Accept(offer []byte) (answer []byte) {
+	uuid := uuid.New()
+	connIdArray := [len(uuid)]byte(uuid)
+	connId := connIdArray[:]
+	answer, err := demo.server.Accept(connId, offer, demo.callbacks)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+	demo.ctrlStream, _ = demo.server.NewStream(connId, 1, Mode_kSemiReliableOrdered, Priority_kNormal, 0)
+	demo.dataStream, _ = demo.server.NewStream(connId, 2, Mode_kSemiReliableOrdered, Priority_kNormal, 0)
+	return answer
 }
